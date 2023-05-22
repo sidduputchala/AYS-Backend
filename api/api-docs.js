280aed04aca0256aa099d866238f7c41412b3d6d
siddu <sidduputchala@gmail.com>
2023-05-22 17:18:18 +0530

    swagger

---
+++ api/api-docs.js
@@ -0,0 +1,37 @@
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerDocument = require('../swagger.json'); // Your Swagger specification file
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "AT YOUR SERVICE (AYS)",
+            version: "1.0.0",
+        },
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: "http",
+                    scheme: "bearer",
+                    bearerFormat: "JWT",
+                },
+            },
+        },
+        security: [
+            {
+                bearerAuth: [],
+            },
+        ],
+        servers: [
+            {
+                url: process.env.URL,
+            },
+        ],
+    },
+    apis: ["./index.js"],
+};
+const swaggerSpec = swaggerJsDoc(options);
+
+module.exports = (req, res) => {
+  swaggerUi.setup(swaggerSpec)(req, res);
+};
